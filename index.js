c05600a3947d0bf5e32db551fb3f488c177b6a23
quytechabhinav <kashyapabhinav2013@gmail.com>
2023-03-27 11:39:41 +0530

    add new function using openai

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 const axios = require('axios');
-
+//using opeiai
+const openai = require('openai');
+const apiKey = process.env.OPENAI_API_KEY;
+const engine = 'curie-instruct-beta';
 
 async function generateChatGPTResponse(prompt) {
   const response = await axios.post('https://api.openai.com/v1/engines/curie/completions', {
@@ -11,14 +14,37 @@ async function generateChatGPTResponse(prompt) {
   }, {
     headers: {
       'Content-Type': 'application/json',
-      'Authorization': `Bearer ${process.env.OPENAI_API_KEY}`
+      'Authorization': `Bearer ${apiKey}`
     }
   });
 
   return response.data.choices[0].text.trim();
 }
 
+/**
+ * Using OpenAI
+ * @param {*} message 
+ * @returns 
+ */
+async function generateChatGPTOpenAIResponse(message) {
+  const chatLog = [];
+  const context = chatLog.map((turn) => turn.response).join('\n');
+  const completions = await openai.complete({
+    apiKey,
+    engine,
+    prompt: message,
+    maxTokens: 150,
+    n: 1,
+    context,
+  });
+  const { choices } = completions.data;
+  const response = choices[0].text.trim();
+  chatLog.push({ message, response });
+  return response;
+}
+
 
 module.exports = {
     generateChatGPTResponse,
+    generateChatGPTOpenAIResponse,
 };
